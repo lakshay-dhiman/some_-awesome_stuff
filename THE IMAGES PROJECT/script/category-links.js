8ae80bef237c169dde83602fd2f8d1566bdae7a6
lakshay-dhiman <64729897+lakshay-dhiman@users.noreply.github.com>
2020-05-25 16:13:33 +0530

    Add files via upload

---
+++ THE IMAGES PROJECT/script/category-links.js
@@ -0,0 +1,14 @@
+var category;
+
+document.addEventListener("click",function(event){
+    var clicked_elem= event.target;
+    var elem_id= clicked_elem.getAttribute("id");
+    var elem_class= clicked_elem.getAttribute("class");
+    console.log(elem_id,elem_class);
+    if(elem_class=="category-links"){
+        category= elem_id;
+        localStorage.setItem("category",category);
+        console.log(category);
+    }
+
+})
