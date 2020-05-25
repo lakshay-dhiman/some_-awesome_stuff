6c30ddd27922c408f5ef34509dd9d3fe79cc15fc
lakshay-dhiman <lakshaydhiman12@outlook.com>
2020-05-25 23:14:53 +0530

    commit

--- THE IMAGES PROJECT/script/category-links.js
+++ THE IMAGES PROJECT/script/category-links.js
@@ -4,7 +4,6 @@ document.addEventListener("click",function(event){
     var clicked_elem= event.target;
     var elem_id= clicked_elem.getAttribute("id");
     var elem_class= clicked_elem.getAttribute("class");
-    console.log(elem_id,elem_class);
     if(elem_class=="category-links"){
         category= elem_id;
         localStorage.setItem("category",category);
